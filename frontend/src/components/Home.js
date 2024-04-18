689eb78167cada9d4fcaf55834eaca82656b9445
wizardyash <modi.yash18042004@gmail.com>
2024-04-18 10:32:37 +0530

    major changes in home

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -4,10 +4,9 @@ import logo from '../images/logo2.jpg';
 import './css/home.css'
 
 export default function Home(){
-  const [hideRegisterButton, setHideRegisterButton] = useState(false);
-  let stockPrice;
+  // let stockPrice;
   const Nifty50StockBox = () => {
-    // State to store the current stock price
+    
     const [stockPrice, setStockPrice] = useState(null);
   
     // Fetch Nifty 50 stock price from an API
@@ -22,24 +21,21 @@ export default function Home(){
           console.error('Error fetching stock price:', error);
         }
       };
-  
       fetchStockPrice();
     }, []);
   }
-
     return(
       <div className='container-home'>
          <div className='nav_container'>
       <ul className='nav_list'>
         <li><Link to='/'><img src={logo} alt='logo'/></Link></li>
-        {!(hideRegisterButton) &&(
+        
         <li className='buttons'>
-          <Link to='/login' id='login' onClick={() => setHideRegisterButton(true)}><span>Login</span></Link>
-            <Link to='/register' id='register' onClick={() => setHideRegisterButton(true)}>
-              <span>Register</span>
-            </Link>
+          <Link to='/login' id='login'><span>Login</span></Link>
+          <Link to='/register' id='register'>
+          <span>Register</span>
+          </Link>
         </li>
-        )}
       </ul>
     </div>
         <div class="animated-title">
--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,14 +1,10 @@
 import React from 'react'
 import './css/register.scss'
+import './css/register.css'
 export default function Register(){
     return(
         <div className='container-register'>
         <div class="stars">
-  {/* <div class="star"></div>
-  <div class="star"></div>
-  <div class="star"></div>
-  <div class="star"></div>
-  <div class="star"></div> */}
   <div class="star"></div>
   <div class="star"></div>
   <div class="star"></div>
@@ -26,12 +22,6 @@ export default function Register(){
   <div class="star"></div>
   <div class="star"></div>
   <div class="star"></div>
-  {/* <div class="star"></div>
-  <div class="star"></div>
-  <div class="star"></div>
-  <div class="star"></div>
-  <div class="star"></div>
-  <div class="star"></div> */}
   <div class="star"></div>
   <div class="star"></div>
   <div class="star"></div>
@@ -39,15 +29,6 @@ export default function Register(){
   <div class="star"></div>
   <div class="star"></div>
   <div class="star"></div>
-  {/* <div class="star"></div>
-  <div class="star"></div>
-  <div class="star"></div>
-  <div class="star"></div>
-  <div class="star"></div>
-  <div class="star"></div>
-  <div class="star"></div>
-  <div class="star"></div>
-  <div class="star"></div> */}
   <div class="star"></div>
   <div class="star"></div>
   <div class="star"></div>
@@ -55,6 +36,24 @@ export default function Register(){
   <div class="star"></div>
   <div class="star"></div>
 </div>
+<div className="register-container">
+      <h2>Register</h2>
+      <form className="register-form">
+        <div className="form-group-1">
+          <label htmlFor="username">Username</label>
+          <input type="text" id="username" name="username" placeholder="Enter your username" />
+        </div>
+        <div className="form-group-2">
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" name="email" placeholder="Enter your email" />
+        </div>
+        <div className="form-group-3">
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" name="password" placeholder="Enter your password" />
+        </div>
+        <button type="submit">Register</button>
+      </form>
+    </div>
 </div>
     )
 }
